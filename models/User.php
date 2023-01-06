3dd830f1e3bb9bf0309ec359f1d8dfeb24e76343
Bartosz Wilki <b.wilki0109@gmail.com>
2023-01-06 15:00:54 +0100

    add registration action

--- models/User.php
+++ models/User.php
@@ -2,103 +2,81 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use Yii;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ *
+
+ */
+class User extends ActiveRecord implements IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
+  /**
+   * {@inheritdoc}
+   */
+  public static function tableName()
+  {
+    return 'users';
+  }
 
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
+  /**
+   * {@inheritdoc}
+   */
+  public function rules()
+  {
+    return [
+      [['name', 'email', 'password',], 'required'],
+      ['email', 'email'],
+      ['email', 'unique'],
+      [['created_at', 'updated_at'], 'safe'],
+      [['name', 'email', 'password'], 'string', 'max' => 100],
     ];
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function attributeLabels()
+  {
+    return [
+      'id' => 'ID',
+      'name' => 'Name',
+      'email' => 'Email',
+      'password' => 'Password',
+    ];
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentity($id)
-    {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
-    }
+  public static function findIdentity($id)
+  {
+    return static::findOne($id);
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
+  public static function findIdentityByAccessToken($token, $type = null)
+  {
+    return static::findOne(['access_token' => $token]);
+  }
 
-        return null;
-    }
+  public function getId()
+  {
+    return $this->id;
+  }
 
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
+  public function getAuthKey()
+  {
+    return $this->auth_key;
+  }
 
-        return null;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return $this->password === $password;
-    }
+  public function validateAuthKey($auth_key)
+  {
+    return $this->auth_key === $auth_key;
+  }
 }
